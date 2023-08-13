9e44292eb4bcb53f24de2533a5672e3428e06e4a
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-08-13 14:19:57 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-
-// This is the chain your dApp will work on.
-const activeChain = "mumbai";
+import { Pulsechain } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+    <ThirdwebProvider 
+      activeChain={ Pulsechain } 
+      clientId="3ddba2d53cc78fd3d4979f2c31cd238b" // You can get a client id from dashboard settings
     >
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </ThirdwebProvider>
-  );
+  )
 }
 
 export default MyApp;
